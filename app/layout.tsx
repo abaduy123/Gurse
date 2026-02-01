0b7eeff699502d8a2055cd9f2878b50b43ac8c6b
abaduy123 <dgikpp@gmail.com>
2026-02-01 21:41:37 +0300

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata, Viewport } from 'next'
 import { Inter, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import Script from 'next/script'
 
 const _inter = Inter({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
@@ -45,8 +46,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="so" className="scroll-smooth">
+      <head>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+      (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "vap4ude9vg");
+    `,
+          }}
+        />
+      </head>
+
       <body className={`font-sans antialiased`}>
-        
+
         {children}
         <Analytics />
       </body>
